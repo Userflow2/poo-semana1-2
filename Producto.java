593e1b930cbe94c49cdaaee20cc5255f067fe2e7
Userflow2 <wilsoncueva907@gmail.com>
2026-04-25 22:56:57 -0500

    Feat: agrego la clase Prodcuto con sus atributos, constructor y los getters-setters

---
+++ Producto.java
@@ -0,0 +1,33 @@
+public class Producto {
+
+    private String nombre;
+    private double precio;
+
+    Producto() {
+    }
+
+    public Producto(String _nombre, double _precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    
+
+    
+}
