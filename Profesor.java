0cb71280f281f71a8a2b27ec5e5d7f01a2615cfb
wilsoncrack14 <wilsoncueva714@gmail.com>
2026-04-25 23:25:14 -0500

    feat: Agrega la clase Profesor con atributos, constructor, getters y setters

---
+++ Profesor.java
@@ -0,0 +1,30 @@
+public class Profesor {
+
+    private String nombre;
+    private String materia;
+
+    Profesor() {
+    }
+
+    public Profesor(String _nombre, String _materia) {
+        this.nombre = nombre;
+        this.materia = materia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+    
+}
