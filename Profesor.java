5fae89c6f72872ee848f494c994f6e2cebe2bfec
wilsoncrack14 <wilsoncueva714@gmail.com>
2026-04-25 23:41:20 -0500

    change: agregué validaciones en el constructor y el método toString(). También corregí el naming de variables. Quedo atento a más observaciones.

--- Profesor.java
+++ Profesor.java
@@ -1,14 +1,28 @@
 public class Profesor {
 
+    private int id; 
     private String nombre;
     private String materia;
+    private String correo;
+    private String especialidad;
 
     Profesor() {
     }
 
-    public Profesor(String _nombre, String _materia) {
-        this.nombre = nombre;
-        this.materia = materia;
+    public Profesor(int _id, String _nombre, String _materia, String _correo, String _especialidad) {
+        setId(id);
+        setNombre(nombre);
+        setMateria(materia);
+        setCorreo(correo);
+        setEspecialidad(especialidad);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNombre() {
@@ -26,5 +40,26 @@ public class Profesor {
     public void setMateria(String materia) {
         this.materia = materia;
     }
-    
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Profesor [id=" + id + ", nombre=" + nombre + ", materia=" + materia + ", correo=" + correo
+                + ", especialidad=" + especialidad + "]";
+    } 
 }
